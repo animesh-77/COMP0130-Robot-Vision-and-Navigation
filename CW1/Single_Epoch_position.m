9585b06a1bd78e4b6265139979f82c41588fd31c
animesh-UCL <animesh.srivastava.23@ucl.ac.uk>
2024-02-07 21:53:33 +0000

    docs: add comments for code

--- CW1/Single_Epoch_position.m
+++ CW1/Single_Epoch_position.m
@@ -1,20 +1,23 @@
 function [est_state] = Single_Epoch_position(time, ...
-    pred_r_e_ea, ~, sat_index, obs_prange_a_sati)
+    pred_pos_e_ea, ~, sat_index, obs_prange_a_sati)
 % 
 % 
-% Function to get single epoch position solution without any priors
-% Iterates until position solution does not improve by more than 10cm in any axis
+% Function to get single epoch position solution with OR without any priors
+% Iterates until position solution does not improve by more than 10cm in x y z axis in ECEF coordinate system
+% 
+% 
 % 
 % Inputs :
-% time_index : index of time in the pseudo_range.csv file (1, 1)
 % time : time in seconds (1, 1)
-% r_cap_e_ea_old : prior position of the antenna. Can also be centre of earth (3, 1)  
-% ~ : velocity vector not required . (3, 1)
-% sat_index : index number of satellites 
-% obs_prange_a_sati : observed pseudo range measurements from all satellites
+% pred_pos_e_ea : prior position of the antenna. If not available then coordinates for centre of the Earth is passed (3, 1)  
+% ~ : velocity vector not required (3, 1)
+% sat_index : index number of satellites (n,) where n is the number of satellites
+% obs_prange_a_sati : observed pseudo range measurements from all satellites (n, 1)
+% 
+% 
 % 
 % Returns
-% x_cap_new : position of the antenna
+% est_state : position of the antenna in ECEF coordinate system
 
 
 
@@ -23,107 +26,92 @@ Define_Constants
 
 pred_clock_offset= 1;
 
-% data = readmatrix(filename);
-
-
-%    Where i = index for satellite
-% r_cap_e_esati_old = position for satellite i
-%    Where i = index for satellite
-% v_cap_e_esati_old = velocity for satellite i
 
 sat_count= numel(sat_index);
+% total number of satellites
 
-est_r_e_esati= zeros(sat_count, 3);
-est_v_e_esati= zeros(sat_count, 3);
+est_pos_e_esati= zeros(sat_count, 3);
+% position of all satellites in ECEF
+
+est_vel_e_esati= zeros(sat_count, 3);
+% Velocity of all satellites in ECEF
 
 for index = 1: sat_count
-    [est_r_e_esati(index, :) , est_v_e_esati(index, :)]= ...
+    [est_pos_e_esati(index, :) , est_vel_e_esati(index, :)]= ...
         Satellite_position_and_velocity(time, sat_index(index));
 end
 
-% est_r_e_esati is a number of satellites x 3 matrix
 
 
-% rho_obs_sati_a where i is index of satellite
-    
-% obs_prange_a_sati = data(time_index+1, 2:end)';
-% only read the row at time step required
-
-
-i= 0;
 while 1>0
+% infinite loop unless be break out
         
-    %  where i is the index of the satellite
-    % r_cap_e_asati_corr_old
-    % 
-    %  where i is the index of the satellite
-    % u_e_asati
+ 
     
-    pred_r_e_asati_corr= zeros(sat_count, 1);
+    pred_range_asati= zeros(sat_count, 1);
+    % predicted pseudo ranges from current estimate of antenna position and
+    % satellites
     
     u_e_asati= zeros(sat_count, 3);
+    % line-of-sight unit vector from current estimate of antenna position and
+    % satellites
     
     
     for index = 1: numel(sat_index)
 
-        [pred_r_e_asati_corr(index, 1), u_e_asati(index, :)]= ...
-            line_of_sight_vector(est_r_e_esati(index, :)', pred_r_e_ea);
+        [pred_range_asati(index, 1), u_e_asati(index, :)]= ...
+            line_of_sight_vector(est_pos_e_esati(index, :)', pred_pos_e_ea);
 
     end
     
-    pred_state= [pred_r_e_ea; pred_clock_offset];
-    
-    % fprintf('iteration %d x %-0.3f %-0.3f %-0.3f %-0.3f\n', i, x_cap_old);
-
+    pred_state= [pred_pos_e_ea; pred_clock_offset];
+    % predicted state vector [ pos_x, pos_y pos_z clock offset]
     
     
-    % delta_z_old
     
-    pred_meas_innov= obs_prange_a_sati- pred_r_e_asati_corr - pred_clock_offset;
+    pred_meas_innov= obs_prange_a_sati- pred_range_asati - pred_clock_offset;
+    % predicted measurement innovation vector
     
     
-    % H_e_G
-    % 
-    H_e_G= horzcat(-u_e_asati, ones( sat_count, 1));
+    meas_mat= horzcat(-u_e_asati, ones( sat_count, 1));
+    % measurement matrix
     
-    A= H_e_G'* H_e_G;
+    A= meas_mat'* meas_mat;
     
-    B= inv(A)* H_e_G';
+    B= inv(A)* meas_mat';
     
     C= B* pred_meas_innov;
     
     est_state= pred_state + C;
+    % estimated state using unweighted least-squares
     
 
     x_change= abs(est_state(1)- pred_state(1));
     y_change= abs(est_state(2)- pred_state(2));
     z_change= abs(est_state(3)- pred_state(3));
+    % absolute value of change in position of antenna in ECEF
 
     resultx = x_change < 0.01;
     resulty = y_change < 0.01;
     resultz = z_change < 0.01;
-
-    % if i < printi
-    % 
-    %     fprintf("%d %d %d\n", result1, result2, result3)
-    % end
+    % checking if absolute change in position in less than 10cm
 
 
     if resultx==1 && resulty==1 && resultz==1
-    % if result4 == 1
-        % [m, n] = size(est_state);
+    % If the estimated position solution has not moved by more than 10cm
+    % in any axis we break out and return the current state estimates
         break
     end
 
 
 
 
-    pred_r_e_ea= est_state(1:3, 1);
+    pred_pos_e_ea= est_state(1:3, 1);
     pred_clock_offset= est_state(4, 1);
+    % setting estimated values as predicted values that will be used in
+    % next iteration
 
-    i= i+1;
     
-
 
 end
 
